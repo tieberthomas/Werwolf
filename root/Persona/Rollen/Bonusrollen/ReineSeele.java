0ee6732bcde96269f4e89ec65f107c12c5350e7d
VanillaEGirl <thomas.tieber@gmail.com>
2018-10-12 13:51:48 +0200

    reine seele wird mit magenta hinterlegung im übersichtsscreen angezeigt

--- root/Persona/Rollen/Bonusrollen/ReineSeele.java
+++ root/Persona/Rollen/Bonusrollen/ReineSeele.java
@@ -5,15 +5,20 @@ import root.Persona.Rollen.Constants.BonusrollenType.BonusrollenType;
 import root.Persona.Rollen.Constants.BonusrollenType.Passiv;
 import root.ResourceManagement.ImagePath;
 
+import java.awt.*;
+
 public class ReineSeele extends Bonusrolle {
     public static final String NAME = "Reine Seele";
     public static final String IMAGE_PATH = ImagePath.REINE_SEELE_KARTE;
     public static final BonusrollenType TYPE = new Passiv();
     public boolean dayInvincibility = true;
+    public static final Color COLOR = Color.MAGENTA;
 
     public ReineSeele() {
         this.name = NAME;
         this.imagePath = IMAGE_PATH;
         this.type = TYPE;
+
+        this.color = COLOR;
     }
 }
