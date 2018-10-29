90e0b66c99fe73f47331430219332d6b5505f0d0
VanillaEGirl <thomas.tieber@gmail.com>
2018-10-29 18:47:01 +0100

    miss verona wird jetzt gecalled (ungetestet)

--- root/Persona/Rollen/Hauptrollen/Vampire/MissVerona.java
+++ root/Persona/Rollen/Hauptrollen/Vampire/MissVerona.java
@@ -13,7 +13,7 @@ public class MissVerona extends Hauptrolle {
     public static final String STATEMENT_ID = "MissVerona";
     public static final String STATEMENT_TITLE = "Spieler tarnen";
     public static final String STATEMENT_BESCHREIBUNG = "Miss Verona erwacht und verschleiert die Identität eines Spielers";
-    public static final StatementType STATEMENT_TYPE = StatementType.ROLLE_INFO;
+    public static final StatementType STATEMENT_TYPE = StatementType.ROLLE_CHOOSE_ONE;
 
     public static final String NAME = "Miss Verona";
     public static final String IMAGE_PATH = ImagePath.MISS_VERONA_KARTE;
