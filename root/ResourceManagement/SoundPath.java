b8b259e183043f3751c99840be0b249836a9ffe6
VanillaEGirl <thomas.tieber@gmail.com>
2019-04-19 15:25:00 +0200

    add day and night sounds

--- root/ResourceManagement/SoundPath.java
+++ root/ResourceManagement/SoundPath.java
@@ -2,9 +2,8 @@ package root.ResourceManagement;
 
 class SoundPath {
     private static String SOUND_PATH = "src\\sounds\\";
-    private static String FILE_EXTENSION = ".wav";
 
-    static String CANNON_PATH = SOUND_PATH + "canon" + FILE_EXTENSION;
-    static String FALL_ASLEEP_PATH = SOUND_PATH + "canon" + FILE_EXTENSION;
-    static String WAKE_UP_PATH = SOUND_PATH + "canon" + FILE_EXTENSION;
+    static String CANNON_PATH = SOUND_PATH + "canon.wav";
+    static String FALL_ASLEEP_PATH = SOUND_PATH + "owl.mp3";
+    static String WAKE_UP_PATH = SOUND_PATH + "rooster.mp3";
 }
