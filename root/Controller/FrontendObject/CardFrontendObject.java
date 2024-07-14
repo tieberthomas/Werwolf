6f2ab108d2b8ead29e3b1fc5f53b9317dbecd4da
Stefan Tieber <stefantieber@cloudflight.io>
2024-07-14 17:27:18 +0200

    delete unused frontend objects

--- root/Controller/FrontendObject/CardFrontendObject.java
+++
@@ -1,8 +0,0 @@
-package root.Controller.FrontendObject;
-
-public class CardFrontendObject extends FrontendObject {
-    public CardFrontendObject(String imagePath) {
-        this.typeOfContent = FrontendObjectType.CARD;
-        this.imagePath = imagePath;
-    }
-}
